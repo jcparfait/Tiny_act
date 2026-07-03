d2d574f19317be7890ac494b51fd723e1220b59a
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-03 23:01:00 +0200

    Stabilize mobile ESLint import rules

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -4,4 +4,19 @@ const expoConfig = require("eslint-config-expo/flat");
 module.exports = defineConfig([
   globalIgnores(["dist/*"]),
   expoConfig,
+  {
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+    rules: {
+      "import/default": "off",
+      "import/named": "off",
+      "import/namespace": "off",
+      "import/no-unresolved": "off",
+    },
+  },
 ]);
