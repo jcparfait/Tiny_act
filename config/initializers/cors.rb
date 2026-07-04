0b1743a71305669ee5a007c2eda37ee93bb8e71c
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-04 11:09:58 +0200

    Configure CORS origins by environment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,17 @@
-if Rails.env.development?
+allowed_origins =
+  if Rails.env.development?
+    ["*"]
+  else
+    ENV.fetch("CORS_ORIGINS", "")
+       .split(",")
+       .map(&:strip)
+       .reject(&:blank?)
+  end
+
+if allowed_origins.any?
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "*"
+      origins(*allowed_origins)
 
       resource "/api/*",
                headers: :any,
